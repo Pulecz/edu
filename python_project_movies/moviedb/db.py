b8f900fc1ab0654cbdd30c627059f368ac4fa208
Matěj Týč <matyc@redhat.com>
2018-12-17 14:40:24 +0100

    Added the 12/12 phase of the Python movie DB project.

--- python_project_movies/moviedb/db.py
+++ python_project_movies/moviedb/db.py
@@ -1,3 +1,4 @@
+import json
 
 
 class Film(object):
@@ -6,6 +7,32 @@ class Film(object):
         self.duration = duration
         self.genres = set(genres)
         self.rating = rating
+
+    def to_dict(self):
+        result = dict()
+        result["title"] = self.title
+        result["duration"] = self.duration
+        result["genres"] = list(self.genres)
+        result["rating"] = self.rating
+        return result
+
+    def __eq__(self, rhs):
+        if self.title != rhs.title:
+            return False
+        if self.duration != rhs.duration:
+            return False
+        if self.genres != rhs.genres:
+            return False
+        if self.rating != rhs.rating:
+            return False
+        return True
+
+    @staticmethod
+    def from_dict(d):
+        result = Film(
+            d["title"], d["duration"],
+            d["genres"], d["rating"])
+        return result
 
 
 def create_database():
@@ -23,8 +50,14 @@ def get_film_by_title(db, title):
 
 
 def save_database(db, filename):
+    db_of_dicts = [f.to_dict() for f in db]
+    with open(filename, "w") as save_file:
+        json.dump(db_of_dicts, save_file)
     pass
 
 
 def restore_database(filename):
-    return []
+    with open(filename, "r") as restore_file:
+        db_of_dicts = json.load(restore_file)
+    db = [Film.from_dict(d) for d in db_of_dicts]
+    return db
